f5cf5f39f94bfc114d2b1597ecf8bc39289dc3db
Hari Pratap SN <haripratapsn2003@gmail.com>
2025-08-29 20:05:20 +0530

    Code Added

--- 17.Mutation.py
+++ 17.Mutation.py
@@ -36,3 +36,16 @@
 # abrackdabra
 
 
+def mutate_string(string, position, character):
+    my_list = list(string)
+    my_list[position] = character
+    final_list = "".join(my_list)
+    return final_list
+    
+    
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
