8275da810156090fa8acfb1941de6dc94e682d7c
Hari Pratap SN <haripratapsn2003@gmail.com>
2025-08-30 20:42:34 +0530

    Code Modified

--- 21.Text Wrap.py
+++ 21.Text Wrap.py
@@ -21,13 +21,30 @@
 # The first line contains a string, string.
 # The second line contains the width, max_width
 
+#Method 1
+# import textwrap
 
+# def wrap(string, max_width):
+#     wrapped=textwrap.fill(string,max_width)
+#     return wrapped
+
+# if __name__ == '__main__':
+#     string, max_width = input("Enter the string : "), int(input("Enter the max width: "))
+#     result = wrap(string, max_width)
+#     print(result)
+
+
+#Method 2
 import textwrap
 
 def wrap(string, max_width):
-    wrapped=textwrap.fill(string,max_width)
-    return wrapped
-
+    for i in range(0,len(string)+1,max_width):
+        chunk=string[i:i+max_width]
+        if len(chunk)==max_width:
+            print(chunk)
+        else:
+            return chunk
+    
 if __name__ == '__main__':
     string, max_width = input("Enter the string : "), int(input("Enter the max width: "))
     result = wrap(string, max_width)
