5ae8e30beda3158150ac563ba47a5660d3e08a42
Hari Pratap SN <haripratapsn2003@gmail.com>
2025-09-04 21:29:11 +0530

    Code Added

---
+++ 24.Alphabet Rangoli.py
@@ -0,0 +1,55 @@
+# You are given an integer,N . Your task is to print an alphabet rangoli of size N . (Rangoli is a form of Indian folk art based on creation of patterns.)
+
+# Different sizes of alphabet rangoli are shown below:
+
+# #size 3
+
+# ----c----
+# --c-b-c--
+# c-b-a-b-c
+# --c-b-c--
+# ----c----
+
+# #size 5
+
+# --------e--------
+# ------e-d-e------
+# ----e-d-c-d-e----
+# --e-d-c-b-c-d-e--
+# e-d-c-b-a-b-c-d-e
+# --e-d-c-b-c-d-e--
+# ----e-d-c-d-e----
+# ------e-d-e------
+# --------e--------
+
+# #size 10
+
+# ------------------j------------------
+# ----------------j-i-j----------------
+# --------------j-i-h-i-j--------------
+# ------------j-i-h-g-h-i-j------------
+# ----------j-i-h-g-f-g-h-i-j----------
+# --------j-i-h-g-f-e-f-g-h-i-j--------
+# ------j-i-h-g-f-e-d-e-f-g-h-i-j------
+# ----j-i-h-g-f-e-d-c-d-e-f-g-h-i-j----
+# --j-i-h-g-f-e-d-c-b-c-d-e-f-g-h-i-j--
+# j-i-h-g-f-e-d-c-b-a-b-c-d-e-f-g-h-i-j
+# --j-i-h-g-f-e-d-c-b-c-d-e-f-g-h-i-j--
+# ----j-i-h-g-f-e-d-c-d-e-f-g-h-i-j----
+# ------j-i-h-g-f-e-d-e-f-g-h-i-j------
+# --------j-i-h-g-f-e-f-g-h-i-j--------
+# ----------j-i-h-g-f-g-h-i-j----------
+# ------------j-i-h-g-h-i-j------------
+# --------------j-i-h-i-j--------------
+# ----------------j-i-j----------------
+# ------------------j------------------
+# The center of the rangoli has the first alphabet letter a, and the boundary has the  alphabet letter (in alphabetical order).
+
+def print_rangoli(size):
+    # your code goes here
+    for i in range():
+        pass
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
